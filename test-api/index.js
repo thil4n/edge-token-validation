c5b9fd77c5850cc0578766b7feb94c49ecdea327
Thilan <thil4n@gmail.com>
2025-02-13 08:40:37 +0530

    add test api

---
+++ test-api/index.js
@@ -0,0 +1,29 @@
+// app.js
+
+const express = require('express');
+const app = express();
+
+// Middleware to log request details
+app.use((req, res, next) => {
+    const { method, url, headers, body } = req;
+    const logMessage = `
+    Method: ${method}
+    URL: ${url}
+    Headers: ${JSON.stringify(headers)}
+    Body: ${JSON.stringify(body)}
+  `;
+  
+    console.log(logMessage);
+    next(); // Pass the request to the next middleware
+});
+
+// Sample route
+app.all('/', (req, res) => {
+    res.send('Ack!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
