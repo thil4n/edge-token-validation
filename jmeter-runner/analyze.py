08b74efadd5cca3953ecb07eeef755d8a741ebca
Thilan <thil4n@gmail.com>
2025-03-12 08:19:06 +0530

    refactor: enhance response time output to include minimum and maximum values

--- jmeter-runner/analyze.py
+++ jmeter-runner/analyze.py
@@ -5,5 +5,7 @@ df = pd.read_csv("results.jtl",low_memory=False)
 
 # Calculate average response time
 average_time = df['elapsed'].mean()
+min_time = df['elapsed'].min()
+max_time = df['elapsed'].max()
 
-print(f" Average Response Time: {average_time} ms")
+print(f" Average Response Time: {average_time} ms \n Min Response Time: {min_time} ms \n Max Response Time: {max_time} ms")
