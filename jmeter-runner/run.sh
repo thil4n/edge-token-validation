f354bddf394601fd0d02f67937dc6bba41231b16
Thilan <thil4n@gmail.com>
2025-04-22 09:57:54 +0530

    aws scripts

--- jmeter-runner/run.sh
+++ jmeter-runner/run.sh
@@ -5,10 +5,11 @@
 #  --key-store-file wso2carbon.jks
 
 
-https://ec2-13-50-85-120.eu-north-1.compute.amazonaws.com:8243/netty/1/secured
-https://ec2-13-50-85-120.eu-north-1.compute.amazonaws.com:8243/netty/1/unsecured
-http://ec2-13-48-178-58.eu-north-1.compute.amazonaws.com:8080/
-https://twilight-butterfly-76d6.cloudflare-rnd-eng.workers.dev:443
+# https://ec2-13-50-85-120.eu-north-1.compute.amazonaws.com:8243/netty/1/secured
+# https://ec2-13-50-85-120.eu-north-1.compute.amazonaws.com:8243/netty/1/unsecured
+# http://ec2-13-48-178-58.eu-north-1.compute.amazonaws.com:8080/
+# https://twilight-butterfly-76d6.cloudflare-rnd-eng.workers.dev:443
+# https://twilight-butterfly-76d6.cloudflare-rnd-eng.workers.dev
 
 jmeter -n -t apim-test.jmx \
   -l results.jtl \
@@ -17,10 +18,10 @@ jmeter -n -t apim-test.jmx \
   -Jduration=300 \
   -Jtokens=tokens.csv \
   -Jpayload=payload.json \
-  -Jhost=jwt-test.hacksland.net \
-  -Jport=80 \
-  -Jprotocol=http \
-  -Jpath=/netty/1/unsecured \
+  -Jhost=twilight-butterfly-76d6.cloudflare-rnd-eng.workers.dev \
+  -Jport=443 \
+  -Jprotocol=https \
+  -Jpath=/ \
   -Jresponse_size=200
 
 
